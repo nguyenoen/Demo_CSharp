ee6fdd60282345025deb0e0fbbeae6a0e10649dd
nguyenoen <nguyenoen@hotmail.com>
2024-12-19 08:51:29 +0700

    Add log for Razoz code

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp1.Components;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
@@ -26,3 +26,12 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+
+ILogger<Program> logger = WebApplication.CreateBuilder(args).Build().Services.GetRequiredService<ILogger<Program>>();
+logger.LogCritical("Message"); // Writes a critical message at log level 5
+logger.LogError("Message"); // Writes an error message at log level 4
+logger.LogWarning("Message"); // Writes a warning message at log level 3
+logger.LogInformation("Message"); // Writes an information message at log level 2
+logger.LogDebug("Message"); // Writes a debug message at log level 1
+logger.LogTrace("Message"); // Writes a detailed trace message at log level 0
--- WebApi1/obj/Debug/net9.0/WebApi1.AssemblyInfo.cs
+++ WebApi1/obj/Debug/net9.0/WebApi1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725bd815edde79b6096b920186165a90d19df29e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
